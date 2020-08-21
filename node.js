a79c96257191e87a2d898c0098b9ddca6417c38a
Hazmi35 <contact@hzmi.xyz>
2020-08-21 23:36:00 +0700

    fix: remove some rules

--- node.js
+++ node.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 module.exports = {
     "extends": path.join(__dirname, "index.js"),
-    "env": { node: true },
-    "rules": {
+    env: { node: true },
+    rules: {
         "no-extra-parens": ["warn", "all", { nestedBinaryExpressions: false }],
-        "array-callback-return": "error",
+        "array-callback-return": "warn",
         "object-shorthand": "warn"
     }
 };
