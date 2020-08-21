13511a38f81e98f57387d1c73f7a1efb7b549c9a
Hazmi35 <contact@hzmi.xyz>
2020-08-22 03:35:21 +0700

    fix: use tsconfig from extended cwd

--- typescript/index.js
+++ typescript/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 module.exports = {
     "extends": [path.join(__dirname, "..", "index.js"), "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
     parserOptions: {
-        project: "./tsconfig.json"
+        project: path.join(process.cwd(), "tsconfig.json")
     },
     rules: {
         "@typescript-eslint/adjacent-overload-signatures": "warn",
