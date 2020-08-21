5c440516e4cb7f5410a12638c8850b99114707dc
Hazmi35 <contact@hzmi.xyz>
2020-08-21 21:58:48 +0700

    feat: node env version

---
+++ node.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+const path = require("path");
+
+module.exports = {
+    "extends": path.join(__dirname, "index.js"),
+    "env": { node: true },
+    "rules": {
+        "no-extra-parens": ["warn", "all", { nestedBinaryExpressions: false }],
+        "array-callback-return": "error",
+        "object-shorthand": "warn"
+    }
+};
