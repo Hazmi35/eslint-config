53b53aacdd51f345f9ac99e2b31b3b7f46cb9d2a
Hazmi35 <contact@hzmi.xyz>
2022-01-18 06:40:04 +0700

    feat(main): [max-len] ignore urls strings, templateLiterals, and RegExpLiterals

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { resolve } = require("path");
 const { readFileSync } = require("fs");
 const stripJsonComments = require("strip-json-comments");
 
+// TODO: Why dont just place everything here instead of .eslintrc.json?
 const file = readFileSync(resolve(__dirname, ".eslintrc.json")).toString();
 
 module.exports = JSON.parse(stripJsonComments(file));
