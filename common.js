ed6bab596a6a1600917b93f7a5f787626765c018
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-17 18:58:23 +0700

    feat: extend to @eslint/js recommended

---
+++ common.js
@@ -0,0 +1,9 @@
+import js from "@eslint/js";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+    js.configs.recommended,
+    {
+
+    }
+]
---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import common from './common.js';
+
+export default [...common];
