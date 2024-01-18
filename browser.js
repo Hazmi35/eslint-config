af062dc68e11b6d56543172009c42944e4b0d9e4
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-18 23:57:47 +0700

    feat: add browser config

---
+++ browser.js
@@ -0,0 +1,11 @@
+import globals from "globals";
+
+export default [
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            }
+        }
+    }
+]
