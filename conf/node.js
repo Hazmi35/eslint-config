22aeb249c174759bb3c3cb9492127bed744e780d
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-20 00:49:55 +0700

    fix(node): fix node plugin

--- conf/node.js
+++ conf/node.js
@@ -55,32 +55,32 @@ export default [
             "unicorn/prefer-node-protocol": "error",
             "unicorn/require-post-message-target-origin": "off",
 
-            // eslint-plugin-node
-            "n/callback-return": ["error", ["callback", "cb"]],
-            "n/handle-callback-err": "error",
-            "n/no-callback-literal": "error",
-            "n/no-deprecated-api": "error",
-            "n/no-exports-assign": "error",
-            "n/no-new-require": "error",
-            "n/no-path-concat": "error",
-            "n/no-sync": [
+            // eslint-plugin-n (Mapped to node)
+            "node/callback-return": ["error", ["callback", "cb"]],
+            "node/handle-callback-err": "error",
+            "node/no-callback-literal": "error",
+            "node/no-deprecated-api": "error",
+            "node/no-exports-assign": "error",
+            "node/no-new-require": "error",
+            "node/no-path-concat": "error",
+            "node/no-sync": [
                 "error",
                 {
                     allowAtRootLevel: true,
                 }
             ],
-            "n/no-unpublished-bin": "error",
-            "n/prefer-global/buffer": ["error", "never"],
-            "n/prefer-global/console": ["error", "always"],
-            "n/prefer-global/process": ["error", "never"],
-            "n/prefer-global/text-decoder": ["error", "never"],
-            "n/prefer-global/text-encoder": ["error", "never"],
-            "n/prefer-global/url": ["error", "never"],
-            "n/prefer-global/url-search-params": ["error", "never"],
-            "n/prefer-promises/dns": "error",
-            "n/prefer-promises/fs": "error",
-            "n/process-exit-as-throw": "error",
-            "n/shebang": [
+            "node/no-unpublished-bin": "error",
+            "node/prefer-global/buffer": ["error", "never"],
+            "node/prefer-global/console": ["error", "always"],
+            "node/prefer-global/process": ["error", "never"],
+            "node/prefer-global/text-decoder": ["error", "never"],
+            "node/prefer-global/text-encoder": ["error", "never"],
+            "node/prefer-global/url": ["error", "never"],
+            "node/prefer-global/url-search-params": ["error", "never"],
+            "node/prefer-promises/dns": "error",
+            "node/prefer-promises/fs": "error",
+            "node/process-exit-as-throw": "error",
+            "node/shebang": [
                 "error",
                 {
                     convertPath: {
