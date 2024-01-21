4d2ae6749c108afb8bd8048c49b10d0ac7674d2a
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-21 12:45:54 +0700

    fix(modules): set sourceType to module

--- conf/modules.js
+++ conf/modules.js
@@ -1,6 +1,9 @@
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
     {
+        languageOptions: {
+            sourceType: "module",
+        },
         rules: {
             "unicorn/prefer-module": "error",
             "unicorn/prefer-top-level-await": "warn"
