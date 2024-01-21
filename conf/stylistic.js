a505a0514f158dc88457ca1e09b6344b6cbd669f
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-21 17:32:36 +0700

    feat(stylistic): add more rules, 30 left

--- conf/stylistic.js
+++ conf/stylistic.js
@@ -8,7 +8,7 @@ export default [
         },
         rules: {
             // Disable legacy rules
-            ...stylistic.configs["disable-legacy"].rules,
+            //...stylistic.configs["disable-legacy"].rules,
 
             // Stylistic rules
             "stylistic/array-bracket-newline": ["warn", "consistent"],
@@ -31,6 +31,73 @@ export default [
             "stylistic/implicit-arrow-linebreak": ["warn", "beside"],
             "stylistic/indent": ["error", 4, { SwitchCase: 1 }],
             "stylistic/indent-binary-ops": ["error", 4],
+            "stylistic/key-spacing": ["warn", { beforeColon: false, afterColon: true, mode: "strict" }],
+            "stylistic/keyword-spacing": ["warn", { before: true, after: true }],
+            "stylistic/linebreak-style": ["error", "unix"],
+            "stylistic/lines-around-comment": [
+                "warn",
+                {
+                    "beforeBlockComment": true,
+                    "afterBlockComment": false,
+                    "beforeLineComment": true,
+                    "afterLineComment": false,
+                    "allowBlockStart": true,
+                    "allowBlockEnd": true,
+                    "allowObjectStart": true,
+                    "allowObjectEnd": true,
+                    "allowArrayStart": true,
+                    "allowArrayEnd": true,
+                    "allowClassStart": true,
+                    "allowClassEnd": true,
+                    "afterHashbangComment": true
+                }
+            ],
+            "stylistic/lines-between-class-members": ["warn", "always", { exceptAfterSingleLine: true }],
+            "stylistic/max-len": [
+                "warn",
+                {
+                    code: 180,
+                    tabWidth: 4,
+                    ignoreUrls: true,
+                    ignoreStrings: true,
+                    ignoreTemplateLiterals: true,
+                    ignoreRegExpLiterals: true
+                }
+            ],
+            "stylistic/max-statements-per-line": ["warn", { max: 3 }],
+            "stylistic/member-delimiter-style": [
+                "warn",
+                {
+                    multiline: {
+                        requireLast: true
+                    },
+                    singleline: {
+                        requireLast: true
+                    }
+                }
+            ],
+            "stylistic/multiline-ternary": ["warn", "always-multiline"],
+            "stylistic/new-parens": "warn",
+            "stylistic/newline-per-chained-call": ["warn", { ignoreChainWithDepth: 3 }],
+            "stylistic/no-confusing-arrow": ["warn", { onlyOneSimpleParam: true }],
+            "stylistic/no-extra-parens": [
+                "warn",
+                "all",
+                {
+                    nestedBinaryExpressions: false,
+                    enforceForArrowConditionals: false,
+                    enforceForNewInMemberExpressions: false
+                }
+            ],
+            "stylistic/no-extra-semi": "warn",
+            "stylistic/no-floating-decimal": "error",
+            "stylistic/no-mixed-operators": "error",
+            "stylistic/no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+            "stylistic/no-multi-spaces": ["error", { ignoreEOLComments: true }],
+            "stylistic/no-multiple-empty-lines": ["warn", { max: 1, maxEOF: 1, maxBOF: 1 }],
+            "stylistic/no-trailing-spaces": "warn",
+            "stylistic/no-whitespace-before-property": "error",
+            "stylistic/nonblock-statement-body-position": ["warn", "beside"],
 
             // JSX
             "stylistic/jsx-child-element-spacing": "warn",
