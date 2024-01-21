a7b36ca282b31f0a95fd7216673d6dfe01c4b87e
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-21 21:46:41 +0700

    fix(modules): import unicorn

--- conf/modules.js
+++ conf/modules.js
@@ -1,9 +1,16 @@
+import unicorn from "eslint-plugin-unicorn";
+
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
     {
         languageOptions: {
             sourceType: "module"
         },
+
+        plugins: {
+            unicorn
+        },
+
         rules: {
             "unicorn/prefer-module": "error",
             "unicorn/prefer-top-level-await": "warn"
