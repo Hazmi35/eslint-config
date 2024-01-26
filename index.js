9272d73c0c1fe44e9bd441869bbe4522cc6313a1
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-01-27 00:42:12 +0700

    feat: add extend function helper

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ export { default as node } from "./conf/node.js";
 export { default as prettier } from "./conf/prettier.js";
 export { default as stylistic } from "./conf/stylistic.js";
 export { default as typescript } from "./conf/typescript.js";
+
+/**
+ * @param {import("eslint").Linter.FlatConfig[]} config
+ * @param {{ rule: string, option: ["off" | "warn" | "error", object] }[]} rulesAndOptions
+ * @returns {import("eslint").Linter.FlatConfig[]}
+ * @example extend(config, [{ rule: "no-console", option: ["warn", { allow: ["warn", "error"] }] }])
+ */
+export function extend(config, rulesAndOptions) {
+    return config.map(c => {
+        if (c.rules) {
+            for (const { rule, option } of rulesAndOptions) {
+                c.rules[rule] = option;
+            }
+        }
+        return c;
+    });
+}
