e5c44c5df50f3ba88174ca1d833d68b75461e884
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-07-06 21:55:38 +0700

    feat(common): add rule unicorn/prefer-string-raw -> warn

--- conf/common.js
+++ conf/common.js
@@ -411,6 +411,7 @@ export default [
             "unicorn/prefer-set-has": "warn",
             "unicorn/prefer-set-size": "warn",
             "unicorn/prefer-spread": "error",
+            "unicorn/prefer-string-raw": "warn",
             "unicorn/prefer-string-replace-all": "error",
             "unicorn/prefer-string-slice": "error",
             "unicorn/prefer-string-starts-ends-with": "error",
