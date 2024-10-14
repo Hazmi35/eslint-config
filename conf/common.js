9cc278b4313987416001ae3d3d8411439b9df617
Hazmi Alfarizqi <contact@hzmi.xyz>
2024-10-14 23:08:58 +0700

    feat(common): remove prefer/math-trunc

--- conf/common.js
+++ conf/common.js
@@ -393,7 +393,6 @@ export default [
             "unicorn/prefer-includes": "error",
             "unicorn/prefer-keyboard-event-key": "error",
             "unicorn/prefer-logical-operator-over-ternary": "error",
-            "unicorn/prefer-math-trunc": "error",
             "unicorn/prefer-modern-math-apis": "warn",
             "unicorn/prefer-module": "off", // Add @hazmi35/eslint-config/modules config
             "unicorn/prefer-native-coercion-functions": "error",
